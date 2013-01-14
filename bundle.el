8ee8a52a55245308b7900e2fea8f5d5c36a10aad
tarao <tarao.gnn@gmail.com>
2013-01-14 23:18:15 +0900

    Add license.

--- bundle.el
+++ bundle.el
@@ -1,3 +1,29 @@
+;;; bundle.el --- An el-get wrapper
+
+;; Author: INA Lintaro <tarao.gnn at gmail.com>
+;; URL: https://gist.github.com/4414297
+;; Version: 0.1
+;; Keywords: emacs package install compile
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'el-get)
 (eval-when-compile (require 'cl))
 
@@ -194,3 +220,4 @@ required."
     `(bundle ,feature ,@(list* 'in feature args))))
 
 (provide 'bundle)
+;;; bundle.el ends here
