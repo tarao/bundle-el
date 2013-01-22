8bb5dec2c8acdf749d7a9af06f1aabe22db758d3
tarao <tarao.gnn@gmail.com>
2013-01-22 22:45:18 +0900

    Merge eval-after-load-compile package.

---
+++ eval-after-load-compile.el
@@ -0,0 +1,48 @@
+;;; eval-after-load-compile.el --- Compiling version of eval-after-load
+
+;; Author: INA Lintaro <tarao.gnn at gmail.com>
+;; URL: https://gist.github.com/4414304.git
+;; Version: 0.1
+;; Keywords: emacs compile
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(eval '(eval-when-compile (require 'cl)))
+
+;;;###autoload
+(defmacro eval-after-load-compile (feature &rest form)
+  (declare (indent defun))
+  (let ((feat (if (and (listp feature) (eq (nth 0 feature) 'quote))
+                  (nth 1 feature) feature)))
+    (eval '(eval-when (compile)
+             ;; don't eval after-loads during compilation
+             (let ((after-load-alist nil))
+               (cond ((stringp feat) (load feat t))
+                     ((symbolp feat) (require feat nil t))))))
+    (if (cond ((stringp feat) (locate-library feat))
+              ((symbolp feat) (featurep feat)))
+        ;; byte-compiled version
+        `(eval-after-load ,feature
+           '(funcall ,(byte-compile `(lambda () ,@form))))
+      ;; normal version
+      `(eval-after-load ,feature '(progn ,@form)))))
+
+(provide 'eval-after-load-compile)
+;;; eval-after-load-compile.el ends here
